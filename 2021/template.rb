dc41ed5453e7d7c1c0609e04153354d24b3d2418
oscar bernardini <bernardini687@gmail.com>
2021-12-21 10:27:00 +0100

    change template

--- 2021/template.rb
+++ 2021/template.rb
@@ -1,11 +1,12 @@
-def run(input = data)
-  p input
+def data(data = DATA)
+  @data ||= DATA.readlines chomp: true
 end
 
-def data(data = DATA)
-  @data ||= DATA.readlines
+def run(input = data)
+  p input
 end
 
 puts "(part 1): #{run}"
 
 __END__
+foo
