a8b84cd751f9c4026c642105f64cbc12ee2ba7ec
oscar bernardini <bernardini687@gmail.com>
2022-12-06 18:04:48 +0100

    aoc 2022 🎄

---
+++ 2022/README.md
@@ -0,0 +1,7 @@
+# advent of code 2022
+
+### [day 1](01.rb)
+
+### [day 2](02.rb)
+
+### [day 3](03.rb)
---
+++ 2022/template.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+lines = []
+
+DATA.each_line(chomp: true) do |line|
+  lines << line
+end
+
+puts "part 1: #{lines}"
+puts "part 2: #{lines}"
+
+__END__
+foo
